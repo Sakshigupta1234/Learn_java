9983dbf0c1f68a3a6723bd6ba76c7576909ec348
Sakshi Gupta <sakshigupta0054@gmail.com>
2025-12-22 23:14:11 +0530

    Creating variables for different data tyes & calling methods

---
+++ Day-1/Day1/Student.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Student {
+ String name;
+ int age;
+ char grade;
+ 
+ void printStudentData() {
+	 System.out.println(name+ " "+age+" "+" "+grade);
+ }
+}
---
+++ Day-1/Day1/StudentMain.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Student s2=new Student();
+s2.name="Sakshi";
+s2.age=25;
+s2.grade='A';
+s2.printStudentData();  //Sakshi 25  A
+	}
+
+}
