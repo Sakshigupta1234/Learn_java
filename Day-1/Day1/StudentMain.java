72a90daad1ed085d78747d6fe7240c548ac955cf
Sakshi Gupta <sakshigupta0054@gmail.com>
2025-12-23 23:07:44 +0530

    multiple ways to create methods

--- Day-1/Day1/StudentMain.java
+++ Day-1/Day1/StudentMain.java
@@ -5,10 +5,18 @@ public class StudentMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Student s2=new Student();
-s2.name="Sakshi";
+
+//1) using object reference variables we are calling the method)
+
+/*s2.name="Sakshi"; 
 s2.age=25;
-s2.grade='A';
-s2.printStudentData();  //Sakshi 25  A
+s2.grade='A'; 
+s2.printStudentData(); */  
+
+
+//using other method
+s2.setStudentData(18,"Sakshi",'A');
+s2.printStudentData();
 	}
 
 }
