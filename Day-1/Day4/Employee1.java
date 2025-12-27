138795f7bfb16ce552ffde540056fa8a4b2b163d
Sakshi Gupta <sakshigupta0054@gmail.com>
2025-12-27 18:48:38 +0530

    when main method is not there in class

---
+++ Day-1/Day4/Employee1.java
@@ -0,0 +1,18 @@
+package Day4;
+
+public class Employee1 {
+	int id; //Class level variables
+	String ename;
+	int age;
+	String role;
+	
+	
+	//Methods
+void Display()
+	{
+		System.out.println(id);
+		System.out.println(ename);
+		System.out.println(age);
+	}
+}
+// Note* we can't run this class as main method is missing, & it throws error
