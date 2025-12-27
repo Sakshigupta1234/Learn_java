72be4b2bda27fbd9307def5e2190c85546e33e38
Sakshi Gupta <sakshigupta0054@gmail.com>
2025-12-27 18:51:07 +0530

    when main method calling attributes & methods of other classs

---
+++ Day-1/Day4/EmployeeMain.java
@@ -0,0 +1,15 @@
+package Day4;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee1 e=new Employee1();
+		e.id=10;
+		e.ename="Saurabh";
+		e.age=25;
+		e.role="Engineer";
+		e.Display();
+	}
+
+}
