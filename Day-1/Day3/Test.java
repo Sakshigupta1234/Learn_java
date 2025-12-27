bba2c1fc803efffaef05df3ce62bcdfbfd12e721
Sakshi Gupta <sakshigupta0054@gmail.com>
2025-12-27 18:40:25 +0530

    Class & Objects

---
+++ Day-1/Day3/Test.java
@@ -0,0 +1,30 @@
+package Day3;
+
+public class Test {
+
+	
+	/*Class- collection of attributes & behavior attributes i.e. variables & behavior means methods
+	-->it will not occupy space in the memory
+	-->class is a blueprint or we can say plan ex. we are planning house so based on plan/blueprint we can construct multiple houses /objects
+	-->One class may have multiple objects
+	*/
+	class Employee
+			{
+			
+			//variables;
+			//methods
+			}
+	
+	
+	/*Object is instance of a class i.e. we can create objects through class
+	 -->occupy space in memory
+	 -->we can create multiple objects in a class
+	 -->object is physical entity & objects derived from class
+	 -->objects can be created inside main method not any where
+	//ex. Animal is class kind of group/ Student is class id, age, name these are attributes i.e. Class is virtual entity but they are not physical but comes under particular group */
+	Test t=new Test(); //Syntax of object creation
+	Test t1=new Test();
+
+	
+
+}
